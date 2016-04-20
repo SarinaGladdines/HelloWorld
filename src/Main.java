fdbcf56ee43460052f7a51c3af307e27f795a0f3
Sarina Gladdines <sgladdin1@student.avans.nl>
2016-04-20 15:17:44 +0200

    Hello Alex

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Galaxy");
+		System.out.println("Hello Alex");
 	}
 
 }
